0057dc6a95080e9e38a730b386e89a6910d28ac9
lisa-go <lis-zaa@live.com>
2022-10-25 03:49:21 -0500

    basic add book to array

--- main.js
+++ main.js
@@ -1,9 +1,27 @@
+
 let myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author) {
+    this.title = title;
+    this.author = author;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+Book.prototype = Object.create(Book.prototype);
+
+Book.prototype.addToLibrary = function() {
+    
+    myLibrary.push([this.title, this.author]);
 }
+
+function newBook() {
+    title = prompt("Book Title?");
+    author = prompt("Author?");
+
+    const newBook = new Book(title, author);
+    newBook.addToLibrary();
+
+    console.log (myLibrary);
+}
+
+
+
