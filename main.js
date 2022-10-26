fdce991d4dc572924de5f6dacb3640be70500b20
lisa-go <lis-zaa@live.com>
2022-10-25 23:01:23 -0500

    shows books in a seperate div and line break between author and title

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+const libContainer = document.querySelector("#library-container");
 
-let myLibrary = [];
+
+let myLibrary = [["naruto", "sensei"]];
 
 function Book(title, author) {
     this.title = title;
@@ -13,15 +15,29 @@ Book.prototype.addToLibrary = function() {
     myLibrary.push([this.title, this.author]);
 }
 
+    /* prompting user for new book info */
 function newBook() {
+    
     title = prompt("Book Title?");
     author = prompt("Author?");
 
     const newBook = new Book(title, author);
     newBook.addToLibrary();
 
+    
+    addDisplay();
     console.log (myLibrary);
 }
 
 
+/* creating new divs for each library item */
+function addDisplay() {
+    const bookDisplay = document.createElement("div");
+    bookDisplay.classList.add("bookDisplay");
+    theBook = myLibrary.slice(-1);
+    bookDisplay.innerHTML = theBook[0][0] + "<br>" + theBook[0][1] + "<br>";
+    libContainer.appendChild(bookDisplay);
+}
 
+
+addDisplay();
